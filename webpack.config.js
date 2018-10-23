45a6c47e31798145a2213e1ef8e89e1b181ae82c
tomoyaf <gazimum@gmail.com>
2018-10-23 10:38:30 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devServer: {
+    contentBase: "dist",
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
