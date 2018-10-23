180bc781f62a7b206d756ad5380612a2e2559ce0
tomoyaf <gazimum@gmail.com>
2018-10-24 02:40:40 +0900

    debug

--- src/templates/NotFound404.js
+++ src/templates/NotFound404.js
@@ -3,11 +3,13 @@ import Wrapper from "../atoms/Wrapper";
 import Text from "../molecules/Text";
 import Header from "../organisms/Header";
 
-export default ({ state, actions }) => (
+export default ({ state, actions }) => {
+  console.log(state);
+  return (
   <Wrapper style={{ flexDirection: "column", width: "100%" }}>
     <Header />
     <Wrapper style={{ flexDirection: "column" }}>
       <Text level="XL">404 Not Found</Text>
     </Wrapper>
   </Wrapper>
-);
+);}
